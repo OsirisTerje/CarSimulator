ec412b16b324a2912c08d93a73066da6930d45e9
Terje Sandstrøm <terje@hermit.no>
2020-12-01 22:17:16 +0100

    runsettings

---
+++ Car.Tests/CheckRunTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CarSimulator
+{
+    public class CheckRunTest
+    {
+        [Test]
+        public void CheckIt()
+        {
+            var webAppUrl = TestContext.Parameters["webAppUrl"];
+        }
+
+    }
+}

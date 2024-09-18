065e9ae9dbda8ac17dd758f2c2515ac654b1a43d
Terje Sandstrom <terje@hermit.no>
2024-09-18 04:48:56 +0200

    update

--- Car.Tests/CheckRunTest.cs
+++ Car.Tests/CheckRunTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace CarSimulator.Tests
 {
@@ -9,6 +10,20 @@ namespace CarSimulator.Tests
         {
             var webAppUrl = TestContext.Parameters["webAppUrl"];
             Assert.That(webAppUrl, Is.EqualTo("http://localhost"));
+        }
+
+        [Explicit]
+        [Test]
+        public void CheckExceptionHandling()
+        {
+            try
+            {
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                
+            }
         }
     }
 }
